3991117c6cace76fde8a9c7f27194b9d2615e42f
Jhironsel Diaz <jhironseld@gmail.com>
2022-05-11 00:34:38 -0400

    Inicio de la session 6

--- src/app/services/info-paginas.service.ts
+++ src/app/services/info-paginas.service.ts
@@ -9,11 +9,25 @@ import { InfoPagina } from '../Interfaces/info-pagina.interface';
 export class InfoPaginasService {
 
   info: InfoPagina = {};
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     this.http.get('assets/Data/data-pagina.json')
     .subscribe((resp: InfoPagina) => {
       this.info = resp;
+    });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html5-35838-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe((resp: any) => {
+      this.equipo = resp;
+      console.log(resp);
     });
   }
 }
